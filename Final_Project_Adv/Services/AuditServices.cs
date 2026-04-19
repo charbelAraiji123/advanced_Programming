1dd35985b0dca74244e0840d8a204998b625bed3
John Doe <johndoe@example.com>
2026-04-19 22:55:18 +0300

    Hola

---
+++ Final_Project_Adv/Services/AuditServices.cs
@@ -0,0 +1,41 @@
+﻿using Final_Project_Adv.Domain.Entities;
+using Final_Project_Adv.Infrastructure.Data;
+using System.Text.Json;
+
+namespace Final_Project_Adv.Services
+{
+    
+
+    public class AuditService
+    {
+        private readonly AppDbContext context;
+
+        public AuditService(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task LogAsync(
+            string action,
+            string entityType,
+            int entityId,
+            object? oldValue,
+            object? newValue,
+            int performedById)
+        {
+            var log = new AuditLog
+            {
+                Action = action,
+                EntityType = entityType,
+                EntityId = entityId,
+                OldValue = oldValue != null ? JsonSerializer.Serialize(oldValue) : null,
+                NewValue = newValue != null ? JsonSerializer.Serialize(newValue) : null,
+                PerformedById = performedById,
+                PerformedAt = DateTime.UtcNow
+            };
+
+            context.AuditLog.Add(log);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Final_Project_Adv/Services/IManagerServices.cs
@@ -0,0 +1,33 @@
+﻿using Final_Project_Adv.Domain.DTO;
+
+namespace Final_Project_Adv.Services;
+    public interface IManagerServices
+{
+    Task<UsersDto> CreateUserAsync(CreateUserDto dto);
+    Task<TaskItemDto> CreateTaskAsync(CreateTaskItemDto dto);
+    Task<bool> DeleteTaskAsync(int id);
+    Task<bool> DeleteSubTaskAsync(int id);
+
+    Task<SubtaskDto> CreateSubtaskAsync(CreateSubtaskDto dto);
+    Task<UsersDto> UpdateUserAsync(int id,UpdateUserDto dto);
+
+    Task<IEnumerable<UsersDto>> GetAllUsersAsync();
+    Task<IEnumerable<TaskItemDto>> GetAllTasksAsync();
+
+    Task<IEnumerable<SubtaskDto>> GetAllSubTasksAsync();
+
+    Task<TaskItemDto> UpdateTasksAsync(int id, UpdateTaskDTO dto);
+
+    Task<SubtaskDto> UpdateSubTasksAsync(int id, UpdateSubTaskDTO dto);
+
+    Task<TaskAssignmentDto> TaskAssignAsync(int userId, int taskId);
+
+    Task<TaskAssignmentDto> UnassignTaskAsync(int userId, int taskId);
+
+    Task<UserTaskStatusDto> GetUserTaskStatus(int userId);
+
+    Task<TaskCommentDto> TaskCommentAsync(CreateTaskCommentDto dto);
+    Task<SubtaskCommentDto> SubTaskCommentAsync(CreateSubtaskCommentDto dto);
+
+    Task<List<TaskWithSubtasksDto>> GetOldTasksAsync();
+}
