19fd5c91d1819b242b9ef607165050ddf351977d
John Doe <johndoe@example.com>
2026-05-04 14:58:20 +0300

    hel aan tize ba2a

--- Final_Project_Adv/obj/Debug/net8.0/Final_Project_Adv.AssemblyInfo.cs
+++ Final_Project_Adv/obj/Debug/net8.0/Final_Project_Adv.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final_Project_Adv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce504a95171201ede8e0b88a90138358322e350d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4790486a6b16bff26b6ae795996a6ae2b4ef5d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final_Project_Adv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final_Project_Adv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
